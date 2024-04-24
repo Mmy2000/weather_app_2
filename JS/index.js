5470fd122eec23dd32d499ed79380ff84e7e0283
Mmy2000 <yahianada005@gmail.com>
2024-04-24 13:22:37 +0200

    starting the project

--- JS/index.js
+++ JS/index.js
@@ -1,15 +1,15 @@
 
 // https://api.weatherapi.com/v1/forecast.json?key=ab665c02dc384b38902150930242803&q=london&days=3
 
-async function getData(){
-    let response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=ab665c02dc384b38902150930242803&q=london&days=3`)
+async function getData(q){
+    let response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=ab665c02dc384b38902150930242803&q=${q}&days=3`)
     let finalData = await response.json()
     return finalData;
 }
 
-async function startApp(){
-    let weatherData = await getData()
-    console.log(weatherData.location.name);
+async function startApp(q){
+    let weatherData = await getData(q)
+    console.log(weatherData);
 }
 
 
@@ -19,5 +19,5 @@ async function startApp(){
 
 let searchInput = document.getElementById('searchInput')
 searchInput.addEventListener('input', ()=>{
-    startApp()
+    startApp(searchInput.value)
 })
