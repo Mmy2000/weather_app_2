be870ddf6fe2e95348b890584016bf95a4c803e3
Mmy2000 <yahianada005@gmail.com>
2024-04-24 13:12:42 +0200

    starting the project

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,23 @@
 
 // https://api.weatherapi.com/v1/forecast.json?key=ab665c02dc384b38902150930242803&q=london&days=3
 
+async function getData(){
+    let response = await fetch('https://api.weatherapi.com/v1/forecast.json?key=ab665c02dc384b38902150930242803&q=london&days=3')
+    let finalData = await response.json()
+    return finalData;
+}
+
+async function startApp(){
+    let weatherData = await getData()
+    console.log(weatherData);
+}
+
+
+
+
+
+
+let searchInput = document.getElementById('searchInput')
+searchInput.addEventListener('input', ()=>{
+    startApp()
+})
